d41a25280a1d1a827a83ac08933ca6fb3c450647
Xavier <xavierjohn@hotmail.com>
2023-06-05 17:12:31 -0700

    Empty Projects

---
+++ Application/tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ApplicationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Infrastructure/tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
