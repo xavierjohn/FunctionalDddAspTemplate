ecddc6f6859d22aff95c490966a8539021cc57c4
Xavier <xavierjohn@hotmail.com>
2023-11-03 10:03:42 -0700

    Anti-corruption Layer pattern

--- Infrastructure/tests/Constants.cs
+++ Infrastructure/tests/Constants.cs
@@ -2,5 +2,9 @@
 
 internal class Constants
 {
+    /// <summary>
+    /// Anti-corruption Layer pattern
+    /// https://learn.microsoft.com/en-us/azure/architecture/patterns/anti-corruption-layer
+    /// </summary>
     public const string AntiCorruptionLayerTests = nameof(AntiCorruptionLayerTests);
 }
