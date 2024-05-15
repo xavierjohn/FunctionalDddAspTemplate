baab554c920d30785e10dd79a14ff7735d8d816e
Xavier <xavierjohn@hotmail.com>
2024-05-15 16:33:25 -0700

    Fix filename.

--- Acl/src/EnvironmentOptionsExts/EnvironmentOptionsCosmosDbExt.cs
+++ Acl/src/EnvironmentOptionsExts/EnvironmentOptionsCosmosDbExt.cs
@@ -1,14 +1,6 @@
 ﻿namespace BestWeatherForecast.AntiCorruptionLayer;
 
-public static class EnvironmentOptionsKeyVaultExt
+public static class EnvironmentOptionsCosmosDbExt
 {
-    public static string GetKeyVaultName(this EnvironmentOptions settings) => settings.GetResourceName("kv");
-
-    public static string GetKeyVaultUri(this EnvironmentOptions settings)
-    {
-        if (settings.Cloud == "Public")
-            return $"https://{settings.GetKeyVaultName()}.vault.azure.net/";
-
-        throw new NotImplementedException("Currently, only public cloud is supported.");
-    }
+    public static string GetCosmosDbName(this EnvironmentOptions settings) => settings.GetResourceNameShared("docdb");
 }
--- Acl/src/EnvironmentOptionsExts/EnvironmentOptionsKeyVaultExt.cs
+++ Acl/src/EnvironmentOptionsExts/EnvironmentOptionsKeyVaultExt.cs
@@ -1,6 +1,14 @@
 ﻿namespace BestWeatherForecast.AntiCorruptionLayer;
 
-public static class EnvironmentOptionsCosmosDbExt
+public static class EnvironmentOptionsKeyVaultExt
 {
-    public static string GetCosmosDbName(this EnvironmentOptions settings) => settings.GetResourceNameShared("docdb");
+    public static string GetKeyVaultName(this EnvironmentOptions settings) => settings.GetResourceName("kv");
+
+    public static string GetKeyVaultUri(this EnvironmentOptions settings)
+    {
+        if (settings.Cloud == "Public")
+            return $"https://{settings.GetKeyVaultName()}.vault.azure.net/";
+
+        throw new NotImplementedException("Currently, only public cloud is supported.");
+    }
 }
