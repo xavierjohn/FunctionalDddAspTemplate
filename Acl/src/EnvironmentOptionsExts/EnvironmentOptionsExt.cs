43e3c5b067b6ec53fa63f18998cd27d8f289f5b8
Xavier <xavierjohn@hotmail.com>
2024-11-18 13:23:12 -0800

    Use ToLowerInvariant

--- Acl/src/EnvironmentOptionsExts/EnvironmentOptionsExt.cs
+++ Acl/src/EnvironmentOptionsExts/EnvironmentOptionsExt.cs
@@ -2,6 +2,6 @@
 
 public static class EnvironmentOptionsExt
 {
-    public static string GetResourceName(this EnvironmentOptions settings, string name) => $"{settings.Environment}-{settings.RegionShortName}-{name}-{settings.ServiceName}".ToLower(System.Globalization.CultureInfo.InvariantCulture);
-    public static string GetResourceNameShared(this EnvironmentOptions settings, string name) => $"{settings.Environment}-{name}-{settings.ServiceName}".ToLower(System.Globalization.CultureInfo.InvariantCulture);
+    public static string GetResourceName(this EnvironmentOptions settings, string name) => $"{settings.Environment}-{settings.RegionShortName}-{name}-{settings.ServiceName}".ToLowerInvariant();
+    public static string GetResourceNameShared(this EnvironmentOptions settings, string name) => $"{settings.Environment}-{name}-{settings.ServiceName}".ToLowerInvariant();
 }
