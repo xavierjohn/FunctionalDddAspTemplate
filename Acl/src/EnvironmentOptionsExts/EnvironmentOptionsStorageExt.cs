69e7ac2bae5b74b67a075447491bbf4c83a82252
Xavier <xavierjohn@hotmail.com>
2025-03-13 14:39:27 -0700

    EnvironmentOptionsStorageExt

---
+++ Acl/src/EnvironmentOptionsExts/EnvironmentOptionsStorageExt.cs
@@ -0,0 +1,26 @@
+﻿namespace BestWeatherForecast.AntiCorruptionLayer;
+
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Extension methods for Storage Account EnvironmentOptions.
+/// https://learn.microsoft.com/en-us/azure/storage/common/storage-account-overview
+/// </summary>
+public static partial class EnvironmentOptionsStorageExt
+{
+    public static string GetStorageName(this EnvironmentOptions settings)
+    {
+        var name = settings.GetResourceNameShared("stg");
+        name = GetOnlyLettersAndNumbers().Replace(name, "");
+
+        if (name.Length < 3 || name.Length > 24)
+            throw new ArgumentException("The storage name must be between 3 and 24 characters long.");
+
+        return name;
+    }
+
+    public static string GetBlobStorageUrl(this EnvironmentOptions settings) => $"https://{GetStorageName(settings)}.blob.core.windows.net";
+
+    [GeneratedRegex("[^a-z0-9]")]
+    public static partial Regex GetOnlyLettersAndNumbers();
+}
---
+++ Acl/tests/StorageNameTests.cs
@@ -0,0 +1,67 @@
+﻿namespace AntiCorruptionLayer.Tests;
+
+using BestWeatherForecast.AntiCorruptionLayer;
+using Xunit;
+
+public class StorageNameTests
+{
+    [Theory]
+    [InlineData("local")]
+    [InlineData("test")]
+    public void Will_get_storage_name(string env)
+    {
+        // Arrange
+        EnvironmentOptions environmentOptions = new()
+        {
+            Environment = env,
+            RegionShortName = "usw2",
+            ServiceName = "bwf"
+        };
+
+        var expected = $"{env}stgbwf";
+
+        // Act
+        var actual = environmentOptions.GetStorageName();
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Will_get_blob_storage_url()
+    {
+        // Arrange
+        EnvironmentOptions environmentOptions = new()
+        {
+            Environment = "test",
+            RegionShortName = "usw2",
+            ServiceName = "bwf"
+        };
+
+        var expectedUrl = $"https://teststgbwf.blob.core.windows.net";
+
+        // Act
+        var actualUrl = environmentOptions.GetBlobStorageUrl();
+
+        // Assert
+        actualUrl.Should().Be(expectedUrl);
+    }
+
+    [Fact]
+    public void Will_throw_exception_name_too_long()
+    {
+        // Arrange
+        EnvironmentOptions environmentOptions = new()
+        {
+            Environment = "alongenvironmentthatdoesnotexist",
+            RegionShortName = "usw3",
+            ServiceName = "aservicenamewithasuperlongname"
+        };
+
+        // Act
+        Action act = () => environmentOptions.GetStorageName();
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+}
