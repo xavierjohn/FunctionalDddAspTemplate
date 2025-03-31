139a3c6f8bf41b8ba764c27bef218a7baa359650
Xavier <xavierjohn@hotmail.com>
2025-03-31 13:15:23 -0700

    GetCosmosDbNameShared, GetCosmosDbNameSharedUrl, GetStorageNameShared, GetBlobStorageSharedUrl (#13)
    * EnvironmentOptionsStorageExt
    
    * Update
    
    * GetBlobStorageSharedUrl

--- Acl/src/EnvironmentOptionsExts/EnvironmentOptionsCosmosDbExt.cs
+++ Acl/src/EnvironmentOptionsExts/EnvironmentOptionsCosmosDbExt.cs
@@ -2,5 +2,7 @@
 
 public static class EnvironmentOptionsCosmosDbExt
 {
-    public static string GetCosmosDbName(this EnvironmentOptions settings) => settings.GetResourceNameShared("docdb");
+    public static string GetCosmosDbNameShared(this EnvironmentOptions settings) => settings.GetResourceNameShared("docdb");
+
+    public static string GetCosmosDbNameSharedUrl(this EnvironmentOptions settings) => $"https://{GetCosmosDbNameShared(settings)}.documents.azure.com:443/";
 }
---
+++ Acl/tests/StorageNameTests.cs
@@ -0,0 +1,66 @@
+﻿namespace AntiCorruptionLayer.Tests;
+
+using BestWeatherForecast.AntiCorruptionLayer;
+
+public class StorageNameTests
+{
+    [Theory]
+    [InlineData("local")]
+    [InlineData("test")]
+    public void Will_get_storage_name(string env)
+    {
+        // Arrange
+        EnvironmentOptions environmentOptions = new()
+        {
+            Environment = env,
+            RegionShortName = "usw2",
+            ServiceName = "bwf"
+        };
+
+        var expected = $"{env}stgbwf";
+
+        // Act
+        var actual = environmentOptions.GetStorageNameShared();
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Will_get_blob_storage_url()
+    {
+        // Arrange
+        EnvironmentOptions environmentOptions = new()
+        {
+            Environment = "test",
+            RegionShortName = "usw2",
+            ServiceName = "bwf"
+        };
+
+        var expectedUrl = $"https://teststgbwf.blob.core.windows.net";
+
+        // Act
+        var actualUrl = environmentOptions.GetBlobStorageSharedUrl();
+
+        // Assert
+        actualUrl.Should().Be(expectedUrl);
+    }
+
+    [Fact]
+    public void Will_throw_exception_name_too_long()
+    {
+        // Arrange
+        EnvironmentOptions environmentOptions = new()
+        {
+            Environment = "alongenvironmentthatdoesnotexist",
+            RegionShortName = "usw3",
+            ServiceName = "aservicenamewithasuperlongname"
+        };
+
+        // Act
+        Action act = () => environmentOptions.GetStorageNameShared();
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+}
